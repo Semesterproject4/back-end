4a5c182272123c6168216947c614728d437cb63e
Nichlas Boraso <nibor19@student.sdu.dk>
2021-04-26 12:43:10 +0200

    code-base creation

---
+++ BrewMesModulesParent/common_repository/src/main/java/com/brewmes/common_repository/CommonRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.brewmes.common_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonRepositoryApplication.class, args);
+	}
+
+}
