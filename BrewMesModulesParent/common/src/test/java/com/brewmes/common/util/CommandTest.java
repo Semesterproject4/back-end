cf9da1c9f8ae9b0b3c5bbccb8b0f2840cfe56e9e
Christopher <christopher@kas.net>
2021-04-30 11:58:49 +0200

    Remove obsolete mains and achieved 100% coverage test
    100% coverage test is reached if you run the test alone, otherwise you need to actually call the enum from a bean

--- BrewMesModulesParent/common/src/test/java/com/brewmes/common/util/CommandTest.java
+++ BrewMesModulesParent/common/src/test/java/com/brewmes/common/util/CommandTest.java
@@ -6,31 +6,24 @@ import static org.junit.jupiter.api.Assertions.*;
 class CommandTest {
 
     @Test
-    void assertReset() {
+    void values() {
+        assertNotNull(Command.values());
+    }
+
+    @Test
+    void valueOf() {
         assertEquals(Command.RESET, Command.valueOf("RESET"));
         assertEquals(1, Command.RESET.label);
-    }
 
-    @Test
-    void assertStart() {
         assertEquals(Command.START, Command.valueOf("START"));
         assertEquals(2, Command.START.label);
-    }
 
-    @Test
-    void assertStop() {
         assertEquals(Command.STOP, Command.valueOf("STOP"));
         assertEquals(3, Command.STOP.label);
-    }
 
-    @Test
-    void assertAbort() {
         assertEquals(Command.ABORT, Command.valueOf("ABORT"));
         assertEquals(4, Command.ABORT.label);
-    }
 
-    @Test
-    void assertClear() {
         assertEquals(Command.CLEAR, Command.valueOf("CLEAR"));
         assertEquals(5, Command.CLEAR.label);
     }
