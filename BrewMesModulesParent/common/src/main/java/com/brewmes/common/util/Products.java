3b80936596610734ee505e6ec6135d6212d90bea
Kechr <55458479+Kechr@users.noreply.github.com>
2021-05-03 13:22:14 +0200

    Add Products Enum + test

---
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/util/Products.java
@@ -0,0 +1,18 @@
+package com.brewmes.common.util;
+
+public enum Products {
+    PILSNER("Pilsner", 600),
+    WHEAT("Wheat", 300),
+    IPA("IPA", 150),
+    STOUT("Stout", 200),
+    ALE("Ale", 100),
+    ALCOHOL_FREE("Alcohol Free", 125);
+
+    public final String output;
+    public final int speedLimit;
+
+    Products(String output, int speedLimit) {
+        this.output = output;
+        this.speedLimit = speedLimit;
+    }
+}
