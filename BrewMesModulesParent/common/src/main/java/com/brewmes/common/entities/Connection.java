ec2e497acf6634ee5a8f4c2886cc1e42182bc97f
Teyson <teis@aalbaek-nielsen.dk>
2021-05-03 11:30:36 +0200

    Make Connection entity class
    Made and populated the entity class "Connection"

---
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/entities/Connection.java
@@ -0,0 +1,37 @@
+package com.brewmes.common.entities;
+
+public class Connection {
+    private int id;
+    private String ip;
+    private String name;
+
+    public Connection(int id, String ip, String name) {
+        this.id = id;
+        this.ip = ip;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
