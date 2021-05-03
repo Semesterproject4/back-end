0585bd855cd6a66c8dc23870a24d2bed6af23f31
Christopher <christopher@kas.net>
2021-05-03 15:22:01 +0200

    Add javadoc

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IBatchReportService.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IBatchReportService.java
@@ -1,5 +1,11 @@
 package com.brewmes.common.services;
 
 public interface IBatchReportService {
+
+    /**
+     * Gets a specific batch and formats it for the desired implementation
+     * @param id is a unique string id of a batch
+     * @return the prepared batch report
+     */
     public String prepareBatchReportService(String id);
 }
