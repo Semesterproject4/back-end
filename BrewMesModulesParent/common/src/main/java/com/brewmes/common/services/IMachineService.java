d2f531eb15ae54bbf69e0c589b0c77c3c9db72ea
Teyson <teis@aalbaek-nielsen.dk>
2021-05-03 15:58:42 +0200

    Add return to the javadocs where needed and fix . inconsistencies

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IMachineService.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IMachineService.java
@@ -12,6 +12,7 @@ public interface IMachineService {
      * Takes a command and sends it to the desired machine
      * @param command The command that should be sent to the machine
      * @param machineId The ID of the machine that should receive the command
+     * @return Returns {@code true} if the control command was successfully sent, returns {@code false} if the control command failed
      */
     boolean controlMachine(Command command, String machineId);
 
@@ -21,12 +22,14 @@ public interface IMachineService {
      * @param beerType The desired beer type of the production
      * @param batchSize The desired amount of beer to be produced
      * @param machineId The ID of the desired machine the values should be set on
+     * @return Returns {@code true} if the variables were successfully set, returns {@code false} if the setting of the variables failed
      */
     boolean setVariables(double speed, Products beerType, int batchSize, String machineId);
 
     /**
-     * Sets up the desired subscriptions to a machines values.
+     * Sets up the desired subscriptions to a machines values
      * @param machineId The ID of the machine desired for the subscription
+     * @return Returns {@code true} if the subscription was successfully started, returns {@code false} if the subscription failed
      */
     boolean subscribeToMachineValues(String machineId);
 
@@ -40,26 +43,28 @@ public interface IMachineService {
      * Adds a connection to a Machine
      * @param ip The IP address of the machine
      * @param name The desired name of the machine, used for easy identification of the machine
-     * @return Returns true if the connection was successfully made, returns {@code false} if the connection failed.
+     * @return Returns {@code true} if the connection was successfully made, returns {@code false} if the connection failed
      */
     boolean addConnection(String ip, String name);
 
     /**
      * Removes a connection from the database and from the system in general
      * @param machineId The id of the desired machine
-     * @return Returns true if the connection was successfully removed, returns false if the removal failed.
+     * @return Returns true if the connection was successfully removed, returns false if the removal failed
      */
     boolean removeConnection(String machineId);
 
     /**
-     * Sets the machine in the autobrewing mode so it brews from the queue.
+     * Sets the machine in the autobrewing mode so it brews from the queue
      * @param machineId The id of the machine
+     * @return Returns {@code true} if the autobrewing was successfully started, returns {@code false} if the autobrew starting failed
      */
     boolean startAutoBrew(String machineId);
 
     /**
      * Stops the machine from autobrewing
      * @param machineId The id of the machine
+     * @return Returns {@code true} if the autobrewing was successfully stopped, returns {@code false} if the autobrew stop failed
      */
     boolean stopAutoBrew(String machineId);
 }
