ea76460068c084310ec6dc58db29e78aa11ccad6
Kechr <55458479+Kechr@users.noreply.github.com>
2021-05-03 13:19:22 +0200

    Add MachineState Enum + test

---
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/util/MachineState.java
@@ -0,0 +1,33 @@
+package com.brewmes.common.util;
+
+public enum MachineState {
+    DEACTIVATED("Deactivated", 0),
+    CLEARING("Clearing", 1),
+    STOPPED("Stopped", 2),
+    STARTING("Starting", 3),
+    IDLE("Idle", 4),
+    SUSPENDED("Suspended", 5),
+    EXECUTE("Execute", 6),
+    STOPPING("Stopping", 7),
+    ABORTING("Aborting", 8),
+    ABORTED("Aborted", 9),
+    HOLDING("Holding", 10),
+    HELD("Held", 11),
+    RESETTING("Resetting", 15),
+    COMPLETING("Completing", 16),
+    COMPLETE("Complete", 17),
+    DEACTIVATING("Deactivating", 18),
+    ACTIVATING("Activating", 19);
+
+    public final String output;
+    public final int value;
+
+    MachineState(String output, int value) {
+        this.output = output;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ BrewMesModulesParent/common/src/test/java/com/brewmes/common/util/MachineStateTest.java
@@ -0,0 +1,25 @@
+package com.brewmes.common.util;
+
+import org.junit.jupiter.api.Test;
+
+import javax.crypto.Mac;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MachineStateTest {
+
+    @Test
+    void getValue() {
+        assertEquals(4, MachineState.IDLE.getValue());
+    }
+
+    @Test
+    void values() {
+        assertNotNull(MachineState.values());
+    }
+
+    @Test
+    void valueOf() {
+       assertEquals(MachineState.RESETTING, MachineState.valueOf("RESETTING"));
+    }
+}
