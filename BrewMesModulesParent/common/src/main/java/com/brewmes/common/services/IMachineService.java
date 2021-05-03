b11c0f7164541a4dee513a8bbcf66057ef1d1d56
Teyson <teis@aalbaek-nielsen.dk>
2021-05-03 16:02:51 +0200

    Fixed inconsistent and wrong upper and lower case usage in the word ID

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IMachineService.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IMachineService.java
@@ -11,31 +11,31 @@ public interface IMachineService {
     /**
      * Takes a command and sends it to the desired machine
      * @param command The command that should be sent to the machine
-     * @param machineId The ID of the machine that should receive the command
+     * @param machineID The ID of the machine that should receive the command
      * @return Returns {@code true} if the control command was successfully sent, returns {@code false} if the control command failed
      */
-    boolean controlMachine(Command command, String machineId);
+    boolean controlMachine(Command command, String machineID);
 
     /**
      * Sets the desired values for a production
      * @param speed The desired speed that the production should run at
      * @param beerType The desired beer type of the production
      * @param batchSize The desired amount of beer to be produced
-     * @param machineId The ID of the desired machine the values should be set on
+     * @param machineID The ID of the desired machine the values should be set on
      * @return Returns {@code true} if the variables were successfully set, returns {@code false} if the setting of the variables failed
      */
-    boolean setVariables(double speed, Products beerType, int batchSize, String machineId);
+    boolean setVariables(double speed, Products beerType, int batchSize, String machineID);
 
     /**
      * Sets up the desired subscriptions to a machines values
-     * @param machineId The ID of the machine desired for the subscription
+     * @param machineID The ID of the machine desired for the subscription
      * @return Returns {@code true} if the subscription was successfully started, returns {@code false} if the subscription failed
      */
-    boolean subscribeToMachineValues(String machineId);
+    boolean subscribeToMachineValues(String machineID);
 
     /**
      * Gets all the connections from the database
-     * @return The list of Connection objects gotten from the database
+     * @return The list of {@code Connection} objects gotten from the database
      */
     List<Connection> getConnections();
 
@@ -49,22 +49,22 @@ public interface IMachineService {
 
     /**
      * Removes a connection from the database and from the system in general
-     * @param machineId The id of the desired machine
+     * @param machineID The ID of the desired machine
      * @return Returns true if the connection was successfully removed, returns false if the removal failed
      */
-    boolean removeConnection(String machineId);
+    boolean removeConnection(String machineID);
 
     /**
      * Sets the machine in the autobrewing mode so it brews from the queue
-     * @param machineId The id of the machine
+     * @param machineID The ID of the machine
      * @return Returns {@code true} if the autobrewing was successfully started, returns {@code false} if the autobrew starting failed
      */
-    boolean startAutoBrew(String machineId);
+    boolean startAutoBrew(String machineID);
 
     /**
      * Stops the machine from autobrewing
-     * @param machineId The id of the machine
+     * @param machineID The ID of the machine
      * @return Returns {@code true} if the autobrewing was successfully stopped, returns {@code false} if the autobrew stop failed
      */
-    boolean stopAutoBrew(String machineId);
+    boolean stopAutoBrew(String machineID);
 }
