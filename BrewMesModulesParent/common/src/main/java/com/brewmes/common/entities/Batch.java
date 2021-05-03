b650742ee4b11a0865c84b3b366b35c6f4db2663
dottore1 <54813290+dottore1@users.noreply.github.com>
2021-05-03 16:31:24 +0200

    implement batch entity (#54)
    * implement batch entity
    
    * implement test of batch entity
    
    * Update Batch.java
    
    * fix id
    
    * fix typo in test getID

---
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/entities/Batch.java
@@ -0,0 +1,88 @@
+package com.brewmes.common.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Batch {
+    private String id;
+    private String connectionID;
+    private int productType;
+    private int amountToProduce;
+    private double oee;
+    private double desiredSpeed;
+    private List<MachineData> data;
+
+
+    public Batch(String connectionID, int productType, int amountToProduce, double desiredSpeed) {
+        this.data = new ArrayList<>();
+
+        this.connectionID = connectionID;
+        this.productType = productType;
+        this.amountToProduce = amountToProduce;
+        this.desiredSpeed = desiredSpeed;
+    }
+
+    public Batch() {
+        
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getConnectionID() {
+        return connectionID;
+    }
+
+    public void setConnectionID(String connectionID) {
+        this.connectionID = connectionID;
+    }
+
+    public int getProductType() {
+        return productType;
+    }
+
+    public void setProductType(int productType) {
+        this.productType = productType;
+    }
+
+    public int getAmountToProduce() {
+        return amountToProduce;
+    }
+
+    public void setAmountToProduce(int amountToProduce) {
+        this.amountToProduce = amountToProduce;
+    }
+
+    public double getOee() {
+        return oee;
+    }
+
+    public void setOee(double oee) {
+        this.oee = oee;
+    }
+
+    public double getDesiredSpeed() {
+        return desiredSpeed;
+    }
+
+    public void setDesiredSpeed(double desiredSpeed) {
+        this.desiredSpeed = desiredSpeed;
+    }
+
+    public List<MachineData> getData() {
+        return data;
+    }
+
+    public void setData(List<MachineData> data) {
+        this.data = data;
+    }
+
+    public void addMachineData(MachineData machineData) {
+        data.add(machineData);
+    }
+}
