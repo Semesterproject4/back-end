862042fe258eca953f746908c8148ff583cf36b5
dottore1 <54813290+dottore1@users.noreply.github.com>
2021-05-05 14:29:02 +0200

    Update BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/ISubscribeService.java
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/ISubscribeService.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/ISubscribeService.java
@@ -15,7 +15,7 @@ public interface ISubscribeService {
     /**
      * Should return the latest {@code MachineData} measured.
      * @param connectionID the connectionID representing the machines connection.
-     * @return Returns {@code MachineData} if there exists a current subscribtion on related to the {@code connectionID},
+     * @return {@code MachineData} if there exists a current subscribtion on related to the {@code connectionID},
      * returns {@code null} if no subscribtions were found for the {@code connectionID}.
      */
 
