d4a9ec1835cd75442c43b0a325bd11e4c4dbbe8e
Laust Christensen <bakkegadelc@gmail.com>
2021-05-07 10:12:36 +0200

    Add formatting

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IScheduleService.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/services/IScheduleService.java
@@ -6,34 +6,39 @@ import java.util.List;
 
 public interface IScheduleService {
 
-    /** Adds a batch to the global queue.
+    /**
+     * Adds a batch to the global queue.
      *
-     * @param speed Speed of the batch.
+     * @param speed    Speed of the batch.
      * @param beerType Beer type of the batch.
-     * @param amount Amount to produce.
+     * @param amount   Amount to produce.
      * @return the placement of the added batch in the queue.
      */
     int addToQueue(int speed, int beerType, int amount);
 
-    /** Removes a batch from the queue.
+    /**
+     * Removes a batch from the queue.
      *
      * @param scheduleID a {@code ScheduledBatch}'s ID.
      */
     void removeFromQueue(int scheduleID);
 
-    /** Looks through the queue and returns it as a {@code List}.
+    /**
+     * Looks through the queue and returns it as a {@code List}.
      *
      * @return Every {@code ScheduledBatch} in the queue.
      */
     List<ScheduledBatch> getQueue();
 
-    /** Gets the entity in the queue.
+    /**
+     * Gets the entity in the queue.
      *
      * @return the next batch to produce.
      */
     ScheduledBatch getFirstInQueue();
 
-    /** Checks if there are any batches in the queue.
+    /**
+     * Checks if there are any batches in the queue.
      *
      * @return {@code True} if there is nothing in the queue; {@code False} if there are batches in the queue.
      */
