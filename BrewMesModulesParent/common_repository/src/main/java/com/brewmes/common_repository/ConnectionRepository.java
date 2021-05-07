64e9087438a721de87c2244ecf69aad81c8a0878
Teis Aalbæk-Nielsen <teis@aalbaek-nielsen.dk>
2021-05-07 10:57:58 +0200

    Update BrewMesModulesParent/common_repository/src/main/java/com/brewmes/common_repository/ConnectionRepository.java
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>

--- BrewMesModulesParent/common_repository/src/main/java/com/brewmes/common_repository/ConnectionRepository.java
+++ BrewMesModulesParent/common_repository/src/main/java/com/brewmes/common_repository/ConnectionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConnectionRepository extends MongoRepository<Connection, String>{
+public interface ConnectionRepository extends MongoRepository<Connection, String> {
 }
