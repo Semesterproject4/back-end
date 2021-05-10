9ed971f50dc91261b0b00b484fb9bb9b16c6e0c4
Kechr <55458479+Kechr@users.noreply.github.com>
2021-05-10 17:29:58 +0200

    26 socketmachinedatacontroller (#75)
    * Add LiveDataController and WebSocketConfig
    
    * Add tests
    
    * Exclude WebSocketConfig from Sonar

---
+++ BrewMesModulesParent/livedata/src/main/java/com/brewmes/livedata/LiveDataController.java
@@ -0,0 +1,33 @@
+package com.brewmes.livedata;
+
+import com.brewmes.common.entities.MachineData;
+import com.brewmes.common.services.ISubscribeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Controller
+public class LiveDataController {
+
+    @Autowired(required = false)
+    private ISubscribeService subscribeService;
+
+    @MessageMapping("/connect/{id}")
+    @SendTo("/topic/{id}/livedata")
+    @CrossOrigin(origins = "*")
+    public MachineData livedata(@DestinationVariable("id") String id) throws InterruptedException {
+
+        MachineData machineData = subscribeService.getLatestMachineData(id);
+
+        if (machineData == null) {
+            subscribeService.subscribeToMachineValues(id);
+            machineData = subscribeService.getLatestMachineData(id);
+        }
+        
+        Thread.sleep(1000);
+        return machineData;
+    }
+}
