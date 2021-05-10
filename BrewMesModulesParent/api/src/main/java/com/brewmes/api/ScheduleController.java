29fbadbae91b17285da95a2428d0869f294a0f3e
Teis Aalbæk-Nielsen <teis@aalbaek-nielsen.dk>
2021-05-10 09:38:45 +0200

    40-ScheduleController (#66)
    * Add controller and make validation
    
    * Add more tests
    
    * Add controller and make validation
    
    * Add more tests
    
    * Fix tests after rebase
    
    * Fix formatting
    
    * Fix test and formatting
    
    * Fix test more
    
    * Change endpoints and how give id/object

---
+++ BrewMesModulesParent/api/src/main/java/com/brewmes/api/ScheduleController.java
@@ -0,0 +1,74 @@
+package com.brewmes.api;
+
+import com.brewmes.common.entities.ScheduledBatch;
+import com.brewmes.common.services.IScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/scheduled")
+public class ScheduleController {
+
+    @Autowired(required = false)
+    IScheduleService scheduleService;
+
+    @PostMapping("/")
+    public ResponseEntity<String> addToQueue(@Valid @RequestBody ScheduledBatch scheduledBatch) {
+        int index = scheduleService.addToQueue(scheduledBatch);
+
+        if (index != -1) {
+            return new ResponseEntity<>("Scheduled Batch added to queue", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Scheduled Batch not added to queue", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> removeFromQueue(@PathVariable String id) {
+        boolean success = scheduleService.removeFromQueue(id);
+
+        if (success) {
+            return new ResponseEntity<>("Scheduled Batch removed from queue", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error: Scheduled Batch removal failed!", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Object> getQueue() {
+        List<ScheduledBatch> list = scheduleService.getQueue();
+
+        if (list != null) {
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error: something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/{id}/prioUp")
+    public ResponseEntity<String> prioritizeUpInQueue(@PathVariable String id) {
+        boolean success = scheduleService.moveUpInQueue(id);
+
+        if (success) {
+            return new ResponseEntity<>("Batch was moved up in the queue", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error: Batch was not reprioritized", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/{id}/prioDown")
+    public ResponseEntity<String> prioritizeDownInQueue(@PathVariable String id) {
+        boolean success = scheduleService.moveDownInQueue(id);
+
+        if (success) {
+            return new ResponseEntity<>("Batch was moved down in the queue", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error: Batch was not reprioritized", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ BrewMesModulesParent/api/src/test/java/com/brewmes/api/ScheduleControllerTest.java
@@ -0,0 +1,90 @@
+package com.brewmes.api;
+
+import com.brewmes.common.entities.ScheduledBatch;
+import com.brewmes.common.services.IScheduleService;
+import com.brewmes.common.util.Products;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ScheduleControllerTest {
+
+    @Mock
+    IScheduleService service;
+
+    @InjectMocks
+    ScheduleController controller;
+
+    @Test
+    void addToQueue() {
+        ScheduledBatch scheduledBatch = new ScheduledBatch(100, Products.ALE, 100);
+        ScheduledBatch scheduledBatch1 = new ScheduledBatch(100, Products.ALCOHOL_FREE, 1);
+
+        Mockito.when(service.addToQueue(scheduledBatch)).thenReturn(1);
+        Mockito.when(service.addToQueue(scheduledBatch1)).thenReturn(-1);
+
+        ResponseEntity<String> response = controller.addToQueue(scheduledBatch);
+        ResponseEntity<String> response1 = controller.addToQueue(scheduledBatch1);
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(500, response1.getStatusCode().value());
+    }
+
+    @Test
+    void removeFromQueue() {
+        Mockito.when(service.removeFromQueue("goodID")).thenReturn(true);
+        Mockito.when(service.removeFromQueue("badID")).thenReturn(false);
+
+        ResponseEntity<String> response = controller.removeFromQueue("goodID");
+        ResponseEntity<String> response1 = controller.removeFromQueue("badID");
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(500, response1.getStatusCode().value());
+    }
+
+    @Test
+    void getQueue() {
+        //Checks the best case scenario where a list is actually returned
+        Mockito.when(service.getQueue()).thenReturn(new ArrayList<ScheduledBatch>());
+        ResponseEntity<Object> response = controller.getQueue();
+        assertEquals(200, response.getStatusCode().value());
+
+        //Checks the worst case scenario where some error has happened in the backend and thereby it has returned null to the controller
+        Mockito.when(service.getQueue()).thenReturn(null);
+        response = controller.getQueue();
+        assertEquals(500, response.getStatusCode().value());
+    }
+
+    @Test
+    void prioritizeUpInQueue() {
+        Mockito.when(service.moveUpInQueue("goodID")).thenReturn(true);
+        Mockito.when(service.moveUpInQueue("badID")).thenReturn(false);
+
+        ResponseEntity<String> response = controller.prioritizeUpInQueue("goodID");
+        ResponseEntity<String> response1 = controller.prioritizeUpInQueue("badID");
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(500, response1.getStatusCode().value());
+    }
+
+    @Test
+    void prioritizeDownInQueue() {
+        Mockito.when(service.moveDownInQueue("goodID")).thenReturn(true);
+        Mockito.when(service.moveDownInQueue("badID")).thenReturn(false);
+
+        ResponseEntity<String> response = controller.prioritizeDownInQueue("goodID");
+        ResponseEntity<String> response1 = controller.prioritizeDownInQueue("badID");
+
+        assertEquals(200, response.getStatusCode().value());
+        assertEquals(500, response1.getStatusCode().value());
+    }
+}
