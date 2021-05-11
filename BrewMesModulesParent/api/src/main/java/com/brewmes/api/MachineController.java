815e27b4e7ac1f1166e7f10ac5a27f3802bec7ec
Wylardt <55134038+Wylardt@users.noreply.github.com>
2021-05-11 08:53:09 +0200

    Issue 39 machine controller (#73)
    * Add MachineController
    
    * Add MachineControllerTest
    
    * Clean dependencies
    
    * Change addConnection to use Connection
    
    * Add Connection no-arg test
    
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>
    Co-authored-by: Kechr <55458479+Kechr@users.noreply.github.com>

---
+++ BrewMesModulesParent/api/src/main/java/com/brewmes/api/MachineController.java
@@ -0,0 +1,106 @@
+package com.brewmes.api;
+
+import com.brewmes.common.entities.Connection;
+import com.brewmes.common.services.IMachineService;
+import com.brewmes.common.util.Command;
+import com.brewmes.common.util.Products;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/machine")
+public class MachineController {
+
+    @Autowired(required = false)
+    private IMachineService machineService;
+
+
+    @GetMapping
+    public ResponseEntity<Object> getConnections() {
+        return new ResponseEntity<>(machineService.getConnections(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Object> getConnection(@PathVariable("id") String id) {
+        Optional<Connection> connection = Optional.ofNullable(machineService.getConnection(id));
+        if (connection.isPresent()) {
+            return new ResponseEntity<>(connection.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Could not find that machine ", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> removeConnection(@PathVariable("id") String id) {
+        if (machineService.removeConnection(id)) {
+            return new ResponseEntity<>("Machine is removed", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Could not remove machine", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> addConnection(@Valid @RequestBody Connection connection) {
+        if (machineService.addConnection(connection)) {
+            return new ResponseEntity<>("Machine is added", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Could not add machine", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<String> controlMachine(@PathVariable("id") String id, @RequestParam String command) {
+        if (machineService.controlMachine(Command.valueOf(command.toUpperCase()), id)) {
+            return new ResponseEntity<>("Command successful", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Command unsuccessful", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Sets variables of the machine.
+     *
+     * @param id    is the {@code machineID}
+     * @param input consists of three variables; {@code speed} ({@code double}), {@code beerType} ({@code String}) and {@code batchSize} ({@code int}).
+     *              These should be mapped as {@code JSON objects}.
+     * @return {@code 200 OK} if successful, {@code 404 NOT FOUND} if unsuccessful.
+     */
+    @PutMapping(value = "/{id}/variables")
+    public ResponseEntity<String> setMachineVariables(@PathVariable("id") String id, @RequestBody String input) {
+        JsonObject jsonObject = JsonParser.parseString(input).getAsJsonObject();
+        double speed = jsonObject.get("speed").getAsDouble();
+        Products beerType = Products.valueOf(jsonObject.get("beerType").getAsString().toUpperCase());
+        int batchSize = jsonObject.get("batchSize").getAsInt();
+
+        if (machineService.setVariables(speed, beerType, batchSize, id)) {
+            return new ResponseEntity<>("Machine Variables set", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Could not set Machine Variables", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "/{id}/autobrew/start")
+    public ResponseEntity<String> startAutoBrew(@PathVariable("id") String id) {
+        if (machineService.startAutoBrew(id)) {
+            return new ResponseEntity<>("Auto Brew started", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to start Auto Brew", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "/{id}/autobrew/stop")
+    public ResponseEntity<String> stopAutoBrew(@PathVariable("id") String id) {
+        if (machineService.stopAutoBrew(id)) {
+            return new ResponseEntity<>("Auto Brew stopped", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to stop Auto Brew", HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ BrewMesModulesParent/api/src/test/java/com/brewmes/api/MachineControllerTest.java
@@ -0,0 +1,186 @@
+package com.brewmes.api;
+
+import com.brewmes.common.entities.Connection;
+import com.brewmes.common.services.IMachineService;
+import com.brewmes.common.util.Command;
+import com.brewmes.common.util.Products;
+import com.google.gson.JsonObject;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class MachineControllerTest {
+
+    private static final List<Connection> connectionList = new ArrayList<>();
+    private static final JsonObject jsonObject = new JsonObject();
+    private static final Connection connection = new Connection("T-800", "1.1.1.1", "Arnold");
+    @Mock
+    IMachineService machineService;
+    @InjectMocks
+    MachineController machineController;
+
+    @BeforeAll
+    static void init() {
+        connectionList.add(new Connection("123", "0.0.0.0", "Machine1"));
+
+        double speed = 50.0;
+        String beerType = "ale";
+        int BatchSize = 100;
+        jsonObject.addProperty("speed", speed);
+        jsonObject.addProperty("beerType", beerType);
+        jsonObject.addProperty("batchSize", BatchSize);
+    }
+
+    @Test
+    void getConnections() {
+        Mockito.when(machineService.getConnections()).thenReturn(connectionList);
+        ResponseEntity<Object> response = machineController.getConnections();
+
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(connectionList, response.getBody());
+    }
+
+    @Test
+    void getConnection_missing() {
+        Mockito.when(machineService.getConnection("789")).thenReturn(null);
+        ResponseEntity<Object> response = machineController.getConnection("789");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void getConnection_present() {
+        Mockito.when(machineService.getConnection("123")).thenReturn(connectionList.get(0));
+
+        ResponseEntity<Object> response = machineController.getConnection("123");
+
+        Connection responseConnection = (Connection) response.getBody();
+        assertEquals(connectionList.get(0), responseConnection);
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void deleteConnection_present() {
+        Mockito.when(machineService.removeConnection("123")).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.removeConnection("123");
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void deleteConnection_missing() {
+        Mockito.when(machineService.removeConnection("123")).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.removeConnection("123");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void addConnection_succes() {
+        Mockito.when(machineService.addConnection(connection)).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.addConnection(connection);
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void addConnection_failure() {
+        Mockito.when(machineService.addConnection(connection)).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.addConnection(connection);
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void controlMachine_success() {
+        Mockito.when(machineService.controlMachine(Command.START, "123")).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.controlMachine("123", "start");
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void controlMachine_failure() {
+        Mockito.when(machineService.controlMachine(Command.STOP, "123")).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.controlMachine("123", "stop");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void setMachineVariables_success() {
+        Mockito.when(machineService.setVariables(
+                jsonObject.get("speed").getAsDouble(),
+                Products.valueOf(jsonObject.get("beerType").getAsString().toUpperCase()),
+                jsonObject.get("batchSize").getAsInt(), "123")).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.setMachineVariables("123", String.valueOf(jsonObject));
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void setMachineVariables_failure() {
+        Mockito.when(machineService.setVariables(
+                jsonObject.get("speed").getAsDouble(),
+                Products.valueOf(jsonObject.get("beerType").getAsString().toUpperCase()),
+                jsonObject.get("batchSize").getAsInt(), "789")).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.setMachineVariables("789", String.valueOf(jsonObject));
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void startAutoBrew_success() {
+        Mockito.when(machineService.startAutoBrew("123")).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.startAutoBrew("123");
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void startAutoBrew_failure() {
+        Mockito.when(machineService.startAutoBrew("789")).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.startAutoBrew("789");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+
+    @Test
+    void stopAutoBrew_success() {
+        Mockito.when(machineService.stopAutoBrew("123")).thenReturn(true);
+
+        ResponseEntity<String> response = machineController.stopAutoBrew("123");
+
+        assertEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void stopAutoBrew_failure() {
+        Mockito.when(machineService.stopAutoBrew("789")).thenReturn(false);
+
+        ResponseEntity<String> response = machineController.stopAutoBrew("789");
+
+        assertEquals(404, response.getStatusCodeValue());
+    }
+}
