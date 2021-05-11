410465f219f7484603f477ae87be30f962fee297
Anders Wylardt <wylardt@live.dk>
2021-05-11 16:15:06 +0200

    Add test Impl

--- BrewMesModulesParent/schedule/src/test/java/com/brewmes/schedule/ScheduleServiceImplTest.java
+++ BrewMesModulesParent/schedule/src/test/java/com/brewmes/schedule/ScheduleServiceImplTest.java
@@ -3,29 +3,39 @@ package com.brewmes.schedule;
 import com.brewmes.common.entities.ScheduledBatch;
 import com.brewmes.common.util.Products;
 import com.brewmes.common_repository.ScheduleRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ScheduleServiceImplTest {
 
-    @Mock ScheduleRepository scheduleRepository;
+    @Mock
+    ScheduleRepository scheduleRepository;
 
-    @InjectMocks ScheduleServiceImpl scheduleService;
+    @InjectMocks
+    ScheduleServiceImpl scheduleService;
 
-    private final ScheduledBatch scheduledBatch = new ScheduledBatch(100, Products.PILSNER, 200);
+    private static ScheduledBatch scheduledBatch;
+
+    @BeforeAll
+    static void setUp() {
+        scheduledBatch = new ScheduledBatch(100, Products.PILSNER, 200);
+    }
 
     @Test
     void addToQueue_success() {
         when(scheduleRepository.findAll().indexOf(scheduledBatch)).thenReturn(1);
 
-        int expected = scheduleRepository.findAll().indexOf(scheduledBatch);
         int actual = scheduleService.addToQueue(scheduledBatch);
 
-        assertEquals(expected, actual);
+        assertEquals(1, actual);
     }
 
     @Test
