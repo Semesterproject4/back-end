ab1d8b98ace8722934c540d990e4fa09d6d04cc7
Nichlas Boraso <nibor19@student.sdu.dk>
2021-05-11 07:57:03 +0200

    implement PdfBatchServiceImpl

---
+++ BrewMesModulesParent/batch/src/main/java/com/brewmes/batch/PdfBatchServiceImpl.java
@@ -0,0 +1,33 @@
+package com.brewmes.batch;
+
+
+import com.brewmes.common.entities.Batch;
+import com.brewmes.common_repository.BatchRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PdfBatchServiceImpl extends PrepareData {
+
+
+    @Autowired(required = false)
+    public BatchRepository batchRepo;
+
+    @Override
+    public String prepareBatchReportService(String id) {
+        Optional<Batch> optional = batchRepo.findById(id);
+        DataOverTime data;
+        String path;
+        if (optional.isPresent()) {
+            data = prepareData(optional.get());
+            PdfReportGenerator.generatePdf(data);   //maybe this method needs to return a String path or at least take one as parameter.
+            path = "realPath";
+        } else {
+            path = "";
+        }
+        return path;
+    }
+}
