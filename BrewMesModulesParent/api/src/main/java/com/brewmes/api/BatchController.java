0b398677a3f180d05a7d2e19dca09fce72152c6d
Teis Aalbæk-Nielsen <teis@aalbaek-nielsen.dk>
2021-05-12 08:27:19 +0200

    61 BatchGetterImpl (#79)
    * Implement BatchGetterImpl and tests
    
    * Change response message
    
    Co-authored-by: Kechr <55458479+Kechr@users.noreply.github.com>
    
    Co-authored-by: Christopher <christopher@kas.net>
    Co-authored-by: Christopher Kas <33216468+Kaszz@users.noreply.github.com>
    Co-authored-by: Kechr <55458479+Kechr@users.noreply.github.com>

--- BrewMesModulesParent/api/src/main/java/com/brewmes/api/BatchController.java
+++ BrewMesModulesParent/api/src/main/java/com/brewmes/api/BatchController.java
@@ -34,6 +34,7 @@ public class BatchController {
 
     /**
      * Gets a paged view of the database
+     *
      * @param page The page that is wanted
      * @param size The size of the page that is wanted
      * @return The {@code ResponseEntity} with the {@code Batches} in a paginated format
@@ -45,45 +46,45 @@ public class BatchController {
 
     /**
      * Gets the object for the Dashboard Batch report
+     *
      * @param id The {@code ID} of the {@code Batch}
      * @return The {@code ResponseEntity} containing the data needed for the Dashboard Batch view
      */
     @GetMapping(value = "/{id}/dashboard")
     public ResponseEntity<String> getBatch(@PathVariable("id") String id) {
-        if (getter.containsId(id)) {
-            return new ResponseEntity<>(dashboardService.prepareBatchReportService(id), HttpStatus.OK);
+        String response = dashboardService.prepareBatchReportService(id);
+
+        if (response != null) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
         } else {
-            return new ResponseEntity<>("No batch found with that id", HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>("Error: Batch not found", HttpStatus.NOT_FOUND);
         }
     }
 
     /**
      * Starts the generation of the PDF report and gets the path for the PDF report
+     *
      * @param id The {@code ID} of the {@code Batch}
      * @return The {@code ResponseEntity} containing the path for the PDF report download
      */
     @GetMapping(value = "/{id}/pdf")
     public ResponseEntity<Object> getBatchPdfReport(@PathVariable("id") String id) {
-        if (getter.containsId(id)) {
-            String fileName = pdfService.prepareBatchReportService(id); //NOSONAR
-            File file = new File(fileName); //NOSONAR
-            InputStreamResource resource = null;
+        String fileName = pdfService.prepareBatchReportService(id); //NOSONAR
+        File file = new File(fileName); //NOSONAR
+        InputStreamResource resource = null;
 
-            try {
-                resource = new InputStreamResource(new FileInputStream(fileName));
+        try {
+            resource = new InputStreamResource(new FileInputStream(fileName));
 
-                HttpHeaders headers = new HttpHeaders(); //NOSONAR
+            HttpHeaders headers = new HttpHeaders(); //NOSONAR
 
-                headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
-                headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-                headers.add("Pragma", "no-cache");
-                headers.add("Expires", "0");
-                return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(MediaType.parseMediaType("application/pdf")).body(resource);
-            } catch (FileNotFoundException e) {
-                return new ResponseEntity<>("Error: File not found", HttpStatus.NOT_FOUND);
-            }
-        } else {
-            return new ResponseEntity<>("No batch found with that id", HttpStatus.NOT_FOUND);
+            headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            return ResponseEntity.ok().headers(headers).contentLength(file.length()).contentType(MediaType.parseMediaType("application/pdf")).body(resource);
+        } catch (FileNotFoundException e) {
+            return new ResponseEntity<>("Error: File not found", HttpStatus.NOT_FOUND);
         }
     }
 }
--- BrewMesModulesParent/api/src/test/java/com/brewmes/api/BatchControllerTest.java
+++ BrewMesModulesParent/api/src/test/java/com/brewmes/api/BatchControllerTest.java
@@ -3,16 +3,16 @@ package com.brewmes.api;
 import com.brewmes.common.entities.Batch;
 import com.brewmes.common.services.IBatchGetterService;
 import com.brewmes.common.services.IBatchReportService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.ResponseEntity;
 
-import java.io.FileNotFoundException;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,6 +32,28 @@ class BatchControllerTest {
     @InjectMocks
     BatchController controller;
 
+    static File file;
+
+    @BeforeAll
+    static void setUp() {
+        file = new File("test.pdf");
+
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @AfterAll
+    static void tearDown() {
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
     @Test
     void getBatches() {
         Mockito.when(getter.getBatches(5, 10)).thenReturn(new ArrayList<Batch>());
@@ -43,36 +65,35 @@ class BatchControllerTest {
 
     @Test
     void getBatch() {
-        Mockito.when(getter.containsId("goodID")).thenReturn(true);
-        Mockito.when(getter.containsId("badID")).thenReturn(false);
-        Mockito.when(dashboardService.prepareBatchReportService("goodID")).thenReturn("whatever");
+        Mockito.when(dashboardService.prepareBatchReportService("id")).thenReturn("text");
 
-        ResponseEntity<String> responseEntity = controller.getBatch("goodID");
+        ResponseEntity<String> responseEntity = controller.getBatch("id");
+
         assertEquals(200, responseEntity.getStatusCode().value());
+    }
 
-        ResponseEntity<String> responseEntity1 = controller.getBatch("badID");
-        assertEquals(404, responseEntity1.getStatusCode().value());
+    @Test
+    void getBatch_wrong_id() {
+        Mockito.when(dashboardService.prepareBatchReportService("id")).thenReturn(null);
+
+        ResponseEntity<String> responseEntity = controller.getBatch("id");
+
+        assertEquals(404, responseEntity.getStatusCode().value());
     }
 
     @Test
     void getBatchPdfReport() {
-        Mockito.when(getter.containsId("goodID")).thenReturn(true);
-        Mockito.when(getter.containsId("badID")).thenReturn(false);
-        Mockito.when(pdfService.prepareBatchReportService("goodID")).thenReturn("batch_report.pdf");
+        Mockito.when(pdfService.prepareBatchReportService("id")).thenReturn("test.pdf");
 
-        ResponseEntity<Object> responseEntity = controller.getBatchPdfReport("goodID");
+        ResponseEntity<Object> responseEntity = controller.getBatchPdfReport("id");
         assertEquals(200, responseEntity.getStatusCode().value());
-
-        ResponseEntity<Object> responseEntity1 = controller.getBatchPdfReport("badID");
-        assertEquals(404, responseEntity1.getStatusCode().value());
     }
 
     @Test
     void getBatchPdfReportBadName() {
-        Mockito.when(getter.containsId("goodID")).thenReturn(true);
-        Mockito.when(pdfService.prepareBatchReportService("goodID")).thenReturn("not_batch_report.pdf");
+        Mockito.when(pdfService.prepareBatchReportService("id")).thenReturn("not_test.pdf");
 
-        ResponseEntity<Object> responseEntity = controller.getBatchPdfReport("goodID");
+        ResponseEntity<Object> responseEntity = controller.getBatchPdfReport("id");
         assertEquals(404, responseEntity.getStatusCode().value());
     }
 }
