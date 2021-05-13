82a674fca129a1508f5aaecf692ebacf904d800b
Christopher Kas <33216468+Kaszz@users.noreply.github.com>
2021-05-13 16:33:38 +0200

    32 MachineServiceImpl (#92)
    * Add MachineServiceImpl along with test and autobrew functionality
    
    * Improved testing
    
    * Remove java doc
    
    Remove java doc from MachineServiceImpl as it already is on the interface
    
    * Remove qualifier from MachineServiceImpl
    
    * Update BrewMesModulesParent/machine/src/main/java/com/brewmes/machine/MachineServiceImpl.java
    
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>
    
    * Fix Kenneths mistake
    
    * Pass services instead of autowire
    
    * Update BrewMesModulesParent/machine/src/main/java/com/brewmes/machine/AutobrewRunner.java
    
    Co-authored-by: Christopher Kas <33216468+Kaszz@users.noreply.github.com>
    
    * Fix enum usage
    
    Co-authored-by: Teyson <teis@aalbaek-nielsen.dk>
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>

---
+++ BrewMesModulesParent/machine/src/main/java/com/brewmes/machine/AutobrewRunner.java
@@ -0,0 +1,43 @@
+package com.brewmes.machine;
+
+import com.brewmes.common.entities.ScheduledBatch;
+import com.brewmes.common.services.IMachineService;
+import com.brewmes.common.services.IScheduleService;
+import com.brewmes.common.services.ISubscribeService;
+import com.brewmes.common.util.Command;
+import com.brewmes.common.util.MachineState;
+
+public class AutobrewRunner implements Runnable {
+    private ISubscribeService subscribeService;
+    private IScheduleService scheduleService;
+    private IMachineService machineService;
+    private String connectionID;
+
+    public AutobrewRunner(String connectionID, ISubscribeService subscribeService, IScheduleService scheduleService, IMachineService machineService) {
+        this.connectionID = connectionID;
+        this.subscribeService = subscribeService;
+        this.scheduleService = scheduleService;
+        this.machineService = machineService;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            MachineState state = subscribeService.getLatestMachineData(connectionID).getState();
+            if (state == MachineState.IDLE) {
+                if (!scheduleService.queueIsEmpty()) {
+                    ScheduledBatch scheduledBatch = scheduleService.takeFirstInQueue();
+                    machineService.setMachineVariables(scheduledBatch.getSpeed(), scheduledBatch.getType(), scheduledBatch.getAmount(), connectionID);
+                    machineService.controlMachine(Command.START, connectionID);
+                }
+            } else if (state == MachineState.COMPLETE) {
+                machineService.controlMachine(Command.RESET, connectionID);
+            } else if (state == MachineState.STOPPED) {
+                machineService.controlMachine(Command.RESET, connectionID);
+            } else if (state == MachineState.ABORTED) {
+                machineService.stopAutoBrew(connectionID);
+                break;
+            }
+        }
+    }
+}
