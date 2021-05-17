afc40b84becab240e1ed98ceb4a4634bec333a92
Wylardt <55134038+Wylardt@users.noreply.github.com>
2021-05-17 19:22:08 +0200

    Add optimalSpeed to Products (#109)

--- BrewMesModulesParent/common/src/main/java/com/brewmes/common/util/Products.java
+++ BrewMesModulesParent/common/src/main/java/com/brewmes/common/util/Products.java
@@ -4,12 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Products {
-    PILSNER(0, 600, "Pilsner"),
-    WHEAT(1, 300, "Wheat"),
-    IPA(2, 150, "IPA"),
-    STOUT(3, 200, "Stout"),
-    ALE(4, 100, "Ale"),
-    ALCOHOL_FREE(5, 125, "Alcohol free");
+    PILSNER(0, 600, 439.64, "Pilsner"),
+    WHEAT(1, 300, 127.74, "Wheat"),
+    IPA(2, 150, 92.32, "IPA"),
+    STOUT(3, 200, 200, "Stout"),
+    ALE(4, 100, 92.75, "Ale"),
+    ALCOHOL_FREE(5, 125, 64.75, "Alcohol free");
 
     private static final Map<Integer, Products> BY_LABEL = new HashMap<>();
 
@@ -21,11 +21,13 @@ public enum Products {
 
     public final int productType;
     public final int speedLimit;
+    public final double optimalSpeed;
     public final String productName;
 
-    Products(int productType, int speedLimit, String name) {
+    Products(int productType, int speedLimit, double optimalSpeed, String name) {
         this.productType = productType;
         this.speedLimit = speedLimit;
+        this.optimalSpeed = optimalSpeed;
         this.productName = name;
     }
 
