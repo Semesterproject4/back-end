017c730a6e9ce2442ff2b8288bff022c1f8eae7a
Christopher <christopher@kas.net>
2021-05-19 12:08:39 +0200

    Add changes required for the front-end

--- BrewMesModulesParent/machine/src/main/java/com/brewmes/machine/AutobrewRunner.java
+++ BrewMesModulesParent/machine/src/main/java/com/brewmes/machine/AutobrewRunner.java
@@ -43,6 +43,7 @@ public class AutobrewRunner implements Runnable {
                 }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+                break;
             }
         }
     }
