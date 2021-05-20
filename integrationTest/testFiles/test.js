f3ab737d9287b4875b6681fc3bc1c37809984e35
dottore1 <54813290+dottore1@users.noreply.github.com>
2021-05-20 11:31:46 +0200

    Integrationtest fix (#135)
    * update test bail configuration
    
    * more options configuration for the test
    
    * logic fix
    
    * remove unnecessary checks from tests

--- integrationTest/testFiles/test.js
+++ integrationTest/testFiles/test.js
@@ -5,15 +5,14 @@ newman.run({
     collection: require('./collection.json'),
     reporters: 'cli',
     iterationCount: 2,
-    bail: true
-}, function (err) {
-    if (err) { 
-        errorFlag = true;
-        throw err; 
-    }
-    if(!errorFlag){
-        console.log('collection run complete!');
-    } else{
-        process.exit(-1);
-    }
-});
+    bail: true,
+    abortOnFailure: true
+}, function (err, summary) {
+        if(summary.run.failures.length > 0){
+            console.log("Collection run failed!");
+            process.exit(-1);
+        }
+        else{
+            console.log('collection run complete! All test passed.');
+        }
+    });
