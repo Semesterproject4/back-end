b4df9c9ecf4e5069fe25d01f46d7efa53712846c
Christopher <christopher@kas.net>
2021-05-23 10:10:28 +0200

    Added brackets to if-statement

--- BrewMesModulesParent/batch/src/main/java/com/brewmes/batch/DataOverTime.java
+++ BrewMesModulesParent/batch/src/main/java/com/brewmes/batch/DataOverTime.java
@@ -19,7 +19,9 @@ public class DataOverTime {
     private double maxHumidity;
 
     public static double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+        if (places < 0) {
+            throw new IllegalArgumentException();
+        }
 
         BigDecimal bd = BigDecimal.valueOf(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
