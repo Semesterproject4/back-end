594913e5dc9e5130ae4016cc0c3b640c90173041
Christopher Kas <33216468+Kaszz@users.noreply.github.com>
2021-05-27 15:44:37 +0200

    Fixed it (#173)
    * Fixed it
    
    * Non-fix fix of test
    
    * Prettier fix
    
    * Add test to publish
    
    
    Co-authored-by: Laust Christensen <bakkegadelc@gmail.com>

--- BrewMesModulesParent/livedata/src/main/java/com/brewmes/livedata/LiveDataController.java
+++ BrewMesModulesParent/livedata/src/main/java/com/brewmes/livedata/LiveDataController.java
@@ -1,5 +1,7 @@
 package com.brewmes.livedata;
 
+import com.brewmes.common.entities.MachineData;
+import com.brewmes.common.services.ILiveDataService;
 import com.brewmes.common.services.ISubscribeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -13,9 +15,16 @@ public class LiveDataController {
     @Autowired
     private ISubscribeService subscribeService;
 
+    @Autowired
+    private ILiveDataService liveDataService;
+
     @MessageMapping("/connect/{id}")
     @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true", allowedHeaders = "*", exposedHeaders = "If-Match")
     public void liveData(@DestinationVariable("id") String id) {
         subscribeService.subscribeToMachineValues(id);
+        MachineData data = subscribeService.getLatestMachineData(id);
+        if (data != null) {
+            liveDataService.publish(data, id);
+        }
     }
 }
