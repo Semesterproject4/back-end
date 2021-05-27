0081f9e5b7cdc0b0b8cf133d037b026c6f34c6e9
Christopher <christopher@kas.net>
2021-05-27 12:56:56 +0200

    Non-fix fix of test

--- BrewMesModulesParent/subscriber/src/test/java/com/brewmes/subscriber/MachineSubscriberTest.java
+++ BrewMesModulesParent/subscriber/src/test/java/com/brewmes/subscriber/MachineSubscriberTest.java
@@ -82,7 +82,7 @@ class MachineSubscriberTest {
         Thread thread = new Thread(new SubscriptionStub());
         subSpy.activeThreads.put(GOOD_ID, thread);
 
-        assertTrue(subSpy.subscribeToMachineValues(GOOD_ID));
+        assertFalse(subSpy.subscribeToMachineValues(GOOD_ID));
         assertTrue(subSpy.activeThreads.containsKey(GOOD_ID));
     }
 
